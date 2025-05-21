fa14142106ee8c1582b0171347f9243159ad4928
Sambhav Saxena <sambhavsaxena02@gmail.com>
2025-05-21 22:22:36 +0530

    fixed health path

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ app.use('/api/analytics', analytics_routes);
 app.use(not_found);
 app.use(error_handler);
 
-app.get("/health", (req, res) => {
-  res.status(200).json({ message: "Server is healthy" });
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "Server is healthy" });
 });
 
 process.on("SIGTERM", async () => {
