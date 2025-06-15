ff0a1ad84741c2f0f55481b3ebdc17e3d4700f94
Sambhav Saxena <sambhavsaxena02@gmail.com>
2025-06-15 23:35:55 +0530

    added limits to file size and number of files

--- routes/queue.js
+++ routes/queue.js
@@ -5,9 +5,28 @@ import get_job_status from "../controllers/queue/status.js";
 import reject_free_user_requests from "../middlewares/api.js"
 
 const router = express.Router();
-const upload = multer({ dest: "uploads/" });
+const upload = multer({
+    dest: "uploads/",
+    limits: {
+        fileSize: 8 * 1024 * 1024, // 5 MB per file
+        files: 5, // max 3 files allowed
+    },
+});
 
-router.route("/enqueue").post(reject_free_user_requests, upload.array("attachments"), enqueue_controller);
+router.route("/enqueue").post(reject_free_user_requests, (req, res, next) => {
+    upload.array("attachments")(req, res, function (err) {
+        if (err?.code === "LIMIT_FILE_SIZE") {
+            return res.status(413).json({ error: "File size exceeds 8MB limit." });
+        }
+        if (err?.code === "LIMIT_FILE_COUNT") {
+            return res.status(413).json({ error: "Maximum 5 files allowed." });
+        }
+        if (err) {
+            return res.status(500).json({ error: "File upload error: " + err.message });
+        }
+        enqueue_controller(req, res, next);
+    });
+});
 router.route("/status").post(reject_free_user_requests, get_job_status);
 
 export default router;
